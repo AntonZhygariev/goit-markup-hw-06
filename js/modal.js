2c2c6cd2831803c1833c200725136a4cf699599c
Anton Zhygariev <anton.zhyharyev@gmail.com>
2020-07-10 20:53:41 +0300

    подписаться в футере

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,14 @@
 		refs.modal.classList.toggle("is-hidden");
 	}
 })();
+
+(() => {
+	document.querySelector(".js-form").addEventListener("submit", e => {
+		e.preventDefault();
+
+		console.log("");
+		console.count("Submit #");
+
+		new FormData(e.currentTarget).forEach((value, name) => console.log(name, ":", value));
+	});
+})();
